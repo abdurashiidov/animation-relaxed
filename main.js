f7e3d2fe502f143c098d6b3e7885dd18c6eaf55a
abdurashiidov <abdurashiidov18@gmail.com>
2021-12-04 09:30:11 +0300

    add math

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let timeHandle;
 const mrandom = Math.random;
 const mfloor = Math.floor;
 const mround = Math.round;
-const mceil =  
+const mceil = Math.ceil 
 
 const mPI = Math.PI;
 const mPIS2 = Math.PI / 2;
