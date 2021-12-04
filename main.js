12dd0e8b6027bd2628102ee6d6019e7c374a4c84
abdurashiidov <abdurashiidov18@gmail.com>
2021-12-04 09:24:26 +0300

    add time

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let widthHandle, heightHandle;
 let xcHandle, ycHandle, zHandle;
 let colorSchemaHandle;
 let colorSchema = 0;
-let 
+let timeHandle
 
 const mPI = Math.PI;
 const mPIS2 = Math.PI / 2;
