8369f2bcdf97f4efc2a8ca78564a2afbcb221a2d
abdurashiidov <abdurashiidov18@gmail.com>
2021-12-04 09:51:55 +0300

    del row

--- main.js
+++ main.js
@@ -152,7 +152,6 @@ function initShadersStuff() {
   let vertexShader = compileShader(vertexSource, gl.VERTEX_SHADER);
   let fragmentShader = compileShader(fragmentSource, gl.FRAGMENT_SHADER);
 
-
   let program = gl.createProgram();
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
