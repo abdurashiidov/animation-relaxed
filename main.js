150dce92ca1ed30555324fe6172fe7217f53e40e
abdurashiidov <abdurashiidov18@gmail.com>
2021-12-04 09:23:46 +0300

    add 0

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let animState;
 let widthHandle, heightHandle;
 let xcHandle, ycHandle, zHandle;
 let colorSchemaHandle;
-let colorSchema = 
+let colorSchema = 0
 
 const mPI = Math.PI;
 const mPIS2 = Math.PI / 2;
